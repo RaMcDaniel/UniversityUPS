ccca5a3336ab8d37b07a4c2d8a106e5c1bdc320e
RaMcDaniel <rmcda35@wgu.edu>
2022-01-13 03:10:35 -0500

    rearrange files

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import csv
 
-import HashTable1
+from UniversityUPS.HashTable1 import HashTable
+
 
 # Read CSV file to get number of packages
+counter = list(csv.reader(open("WGUPS Package File.csv")))
+package_count = len(counter)
+# print(len(counter))
 
-
-# create hashtable object
-
-package_hashtable = HashTable1(40)
+# create hashtable object with size corresponding to number of packages
+package_hashtable = HashTable(package_count)
+package_hashtable.put(1, ["hello", "there"])
+package_hashtable.get(1)

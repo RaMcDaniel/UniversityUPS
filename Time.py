263bba405576180f0fbfa63108ff03724aafd0ec
RaMcDaniel <rmcda35@wgu.edu>
2022-01-22 02:03:42 -0500

    time class broken. many conversion errors

--- Time.py
+++ Time.py
@@ -1,4 +1,5 @@
 import DeliveryAlgorithm
+import datetime
 
 
 class Timing:
@@ -7,16 +8,36 @@ class Timing:
         self.city_map_matrix = city_map_matrix
         self.trip_leg_times = []
 
-    def get_distance_between_addresses(self, package1, package2):
-        if package1 == "hub":
-            package1 ="4001 South 700 East"
-        if package2 == "hub":
-            package2 = "4001 South 700 East"
-        distance = self.city_map_matrix.distance_between_addresses[package1, package2]
-        return distance
+    # def get_distance_between_addresses(self, package1, package2):
+    #    if package1 == "hub":
+     #       package1 ="4001 South 700 East"
+     #   if package2 == "hub":
+     #       package2 = "4001 South 700 East"
+     #   distance = self.city_map_matrix.distance_between_addresses[package1, package2]
+     #   return distance
+
+    def convert_time_to_time_object(self, time):
+        today_date = "01-24-2022 "
+        seconds = "00"
+        combo_time = today_date + time + seconds
+        print(combo_time)
+        time_obj = datetime.datetime.strptime(combo_time, '%m-%d-%Y %H%M%S')
+        # print(date_obj)
+        return time_obj.strftime('%H:%M')
 
     def convert_distance_to_time(self, distance):
-        return distance
+        print(distance)
+        time_in_min = distance/0.3  # 18 miles per hour= 0.3 miles/min
+        print(time_in_min)
+        minutes = round(time_in_min % 60)
+        print(f"minutes {minutes}")
+        hours = round((time_in_min - minutes)/60)
+        print(f"hours {hours}")
+        time_string = f"{hours}{minutes}"
+        print(time_string)
+        time_object = self.convert_time_to_time_object(time_string)
+        print(time_object)
+        return time_object
         # time = distance/18 # 18mph, answers is in hours
 
     def truck3_start_time(self, truck1_end_time, truck2_end_time):
@@ -38,6 +59,9 @@ class Timing:
             print(distance_piece)
             # print(distance_piece_time)
             print(distance_piece_time_sum)
+
+        time_sum = self.convert_distance_to_time(distance_piece_time_sum)
+        print(time_sum)
 
 
 

30cd18af8275ee6bfeafa1d8dedfaae2f0d5b7f5
RaMcDaniel <rmcda35@wgu.edu>
2022-01-22 21:50:39 -0500

    time loop works. next need to add the data pushes to hashtable

--- Time.py
+++ Time.py
@@ -13,11 +13,11 @@ class Timing:
 
     # def get_distance_between_addresses(self, package1, package2):
     #    if package1 == "hub":
-     #       package1 ="4001 South 700 East"
-     #   if package2 == "hub":
-     #       package2 = "4001 South 700 East"
-     #   distance = self.city_map_matrix.distance_between_addresses[package1, package2]
-     #   return distance
+    #       package1 ="4001 South 700 East"
+    #   if package2 == "hub":
+    #       package2 = "4001 South 700 East"
+    #   distance = self.city_map_matrix.distance_between_addresses[package1, package2]
+    #   return distance
 
     def convert_time_to_time_object(self, time):
         today_date = "01-22-2022 "
@@ -29,21 +29,25 @@ class Timing:
         return time_obj
         # time_obj.strftime('%H:%M')   # use this line if you only want HHMM
 
-    def convert_distance_to_time(self, distance):
+    def convert_distance_to_timedelta(self, distance):
         print(distance)
-        time_in_min = distance/0.3  # 18 miles per hour= 0.3 miles/min
+        time_in_min = distance / 0.3  # 18 miles per hour= 0.3 miles/min
         # print(time_in_min)
-        minutes = math.ceil(time_in_min % 60)
-        minutes = format(minutes, '02d')
+        minutes = time_in_min % 60
+        # minutes = math.ceil(time_in_min % 60)
+        # minutes = format(minutes, '02d')
         # print(f"minutes {minutes}")
-        hours = math.ceil((time_in_min - float(minutes))/60)
-        hours = format(hours, '02d')
+        hours = (time_in_min - minutes) / 60
+        # hours = math.ceil((time_in_min - float(minutes))/60)
+        # hours = format(hours, '02d')
+        time_delta = datetime.timedelta(hours=hours, minutes=minutes)
         # print(f"hours {hours}")
-        time_string = f"{hours}{minutes}"
+        # time_string = f"{hours}{minutes}"
         # print(time_string)
-        time_object = self.convert_time_to_time_object(time_string)
+        # time_object = self.convert_time_to_time_object(time_string)
         # print(time_object)
-        return time_object
+        return time_delta
+        # return time_object
         # time = distance/18 # 18mph, answers is in hours
 
     def truck3_start_time(self, truck1_end_time, truck2_end_time):
@@ -62,24 +66,16 @@ class Timing:
         current_time = self.truck_start_time
         for key in nearest_neighbor.ordered_distance_dict:
             distance_piece = nearest_neighbor.ordered_distance_dict[key]
-            distance_piece_time = self.convert_distance_to_time(distance_piece)
+            distance_piece_time = self.convert_distance_to_timedelta(distance_piece)
             print(distance_piece_time)
             # print(type(distance_piece_time))
             print(current_time)
             # print(type(current_time))
-            time_delta = current_time - ??? # distance_piece_time
-            print(time_delta)
-            print(distance_piece)
-            print(distance_piece_time)
-        distance_piece_time_sum += time_delta
+            new_time = current_time + distance_piece_time  # distance_piece_time
+            print(new_time)
+            current_time = new_time
         # current_time = distance_piece_time_sum
-        print(distance_piece_time_sum)
-
-        print(distance_piece_time_sum)
-
-
-
-
+        print(current_time)
 
 
         # do a thing with list from self.truck_route

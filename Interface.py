ba6c75dba568c1ae8353d3c8d85d3c9d3020d5b5
RaMcDaniel <rmcda35@wgu.edu>
2022-01-26 18:18:40 -0500

    documentation progress, references completed

--- Interface.py
+++
@@ -1,7 +0,0 @@
-# C)     Phase I II – Implement Command Line Interface
-# a.  At a minimum, you need a command line interface that will ask the user for a specific time.
-# b.  Once you have the time, display the status of all packages at that specific time as Delivered or Not Delivered.
-# i. utilize the timestamp of the packages in the hash table to determine their delivered state at the particular time.
-# 1. If the timestamp of the package is before the user time, the package is Delivered and vice versa.
-# c.   Ensure that the package status output is a neatly formatted table listed in ascending order by package ID.
-#  i.   You can loop through all package ids and use the hash table lookup() function to retrieve each package.
